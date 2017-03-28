808b297ba9858a9d6b203034018c3debb5e4c313
JackPu <kakashjack@gmail.com>
2017-03-28 10:50:26 +0800

    update style

--- demos/pages/complex-shape.vue
+++ demos/pages/complex-shape.vue
@@ -34,18 +34,17 @@
       <div class="item">
         <svg style="width:400px;height:300px;">
           <defs>
-          <linearGradient id="trunkgrad" x1="0%" y1="0%" x2="0%" y2="100%">
-            <stop offset="0%" stop-color="#996d2e" stop-opacity="1" />
-            <stop offset="100%" stop-color="#d5bc79" stop-opacity="1" />
-          </linearGradient>
-
-          <radialGradient id="shadow" cx="50%" cy="50%" r="50%" fx="50%" fy="50%">
-            <stop offset="0%" style="stop-color:#666666;stop-opacity:0.7" />
-            <stop offset="100%" style="stop-color:#666666;stop-opacity:0" />
-          </radialGradient>
+            <linearGradient id="trunkgrad" x1="0%" y1="0%" x2="0%" y2="100%">
+              <stop offset="0%" stop-color="#996d2e" stop-opacity="1" />
+              <stop offset="100%" stop-color="#d5bc79" stop-opacity="1" />
+            </linearGradient>
+            <radialGradient id="shadow" cx="50%" cy="50%" r="50%" fx="50%" fy="50%">
+              <stop offset="0%" stop-color="#666666"/>
+              <stop offset="100%" stop-color="#cccccc" />
+            </radialGradient>
           </defs>
-          <ellipse cx="188" cy="273" rx="80" ry="20" style="fill:url(#shadow);opacity:0.3;" />
-          <ellipse cx="188" cy="273" rx="24" ry="5" style="fill:url(#shadow);" />
+          <ellipse cx="188" cy="273" rx="80" ry="20" fill="url(#shadow)" />
+          <ellipse cx="188" cy="273" rx="24" ry="5" fill="url(#shadow)" />
           <polygon points="182,211 187,211 187,274 180,274" fill="url(#trunkgrad)" />
           <polygon points="187,211 196,211 198,274 187,274" fill="#996d2e" />
 
@@ -55,14 +54,14 @@
           <polygon points="187,28 216,114 154,162"  fill="#619b37" stroke="#619b37" />
           <polygon points="187,28 216,114 264,162 204,28" fill="#619b37" stroke="#619b37" />
           <polygon points="216,114 264,162 223,210" fill="#54892f" stroke="#54892f" />
-          <polygon points="216,114 223,210 154,162" style="fill:#5c9433" />
+          <polygon points="216,114 223,210 154,162" fill="#5c9433" />
 
           <polygon points="154,162 223,210 188,211" fill="#54892f" stroke="#54892f" />
           <polygon points="154,162 188,211 155,218" fill="#54892f" stroke="#54892f" />
 
           <polygon points="200,70 216,114 185,137" fill="#79b544" stroke="#79b544" />
-          <polygon points="216,114 220,167 185,137" style="fill:#588f32" />
-          <polygon points="216,114 250,148 220,167" style="fill:#598d33" />
+          <polygon points="216,114 220,167 185,137" fill="#588f32" />
+          <polygon points="216,114 250,148 220,167" fill="#598d33" />
 
         </svg>
         <text class="desc">A tree by  Luke Reid</text>
