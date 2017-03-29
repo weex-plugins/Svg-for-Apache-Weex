d3f35cad1e86ba75c6a8ed0358ad9c5fce1a1de2
budao <budao.gs@alibaba-inc.com>
2017-03-29 14:52:39 +0800

    support viewbox

--- android/plugin/src/main/java/com/alibaba/weex/svg/component/WXSvgContainer.java
+++ android/plugin/src/main/java/com/alibaba/weex/svg/component/WXSvgContainer.java
@@ -3,8 +3,10 @@ package com.alibaba.weex.svg.component;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.RectF;
 import android.support.annotation.NonNull;
 import android.text.TextUtils;
 import android.util.Log;
@@ -38,6 +40,7 @@ public class WXSvgContainer extends WXVContainer<WXSvgView> implements ISvgDrawa
   private int mviewBoxWidth = 0;
   private int mviewBoxHeight = 0;
   private WXDomObject mDom;
+  private float mScale;
 
   public WXSvgContainer(WXSDKInstance instance, WXDomObject dom, WXVContainer parent) {
     super(instance, dom, parent);
@@ -49,6 +52,7 @@ public class WXSvgContainer extends WXVContainer<WXSvgView> implements ISvgDrawa
     if (mDom.getStyles().get(Constants.Name.WIDTH) == null) {
       mDom.getStyles().put(Constants.Name.WIDTH, "0");
     }
+    mScale = (float) (WXViewUtils.getScreenWidth(parent.getContext()) * 1.0 / dom.getViewPortWidth());
   }
 
   @Override
@@ -144,11 +148,105 @@ public class WXSvgContainer extends WXVContainer<WXSvgView> implements ISvgDrawa
       mviewBoxHeight = mHeight;
       mviewBoxWidth = mWidth;
     }
-    canvas.save();
-    //canvas.translate(20, 10);
-    canvas.scale(mWidth / mviewBoxWidth, mHeight / mviewBoxHeight, mViewBoxX, mViewBoxY);
+    //canvas.save();
+    float sx = mWidth * 1.0f / mviewBoxWidth;
+    float sy = mHeight * 1.0f / mviewBoxHeight;
+    float scale = Math.min(sx, sy);
+
+    float width = (float) Math.ceil(mviewBoxWidth) * mScale;
+    float height = (float) Math.ceil(mviewBoxHeight) * mScale;
+
+    RectF vbRect = new RectF(mViewBoxX * mScale, mViewBoxY * mScale, (mViewBoxX + mviewBoxWidth) * mScale, (mViewBoxY + mviewBoxHeight) * mScale);
+    RectF eRect = new RectF(0, 0, mWidth * mScale, mHeight * mScale);
+    Matrix mViewBoxMatrix = getTransform(vbRect, eRect, "none", MOS_NONE, false);
+    canvas.concat(mViewBoxMatrix);
     drawElements(canvas);
-    canvas.restore();
+  }
+
+  private static final int MOS_MEET = 0;
+  private static final int MOS_SLICE = 1;
+  private static final int MOS_NONE = 2;
+  static public Matrix getTransform(RectF vbRect, RectF eRect, String align, int meetOrSlice, boolean fromSymbol) {
+    // based on https://svgwg.org/svg2-draft/coords.html#ComputingAViewportsTransform
+
+    // Let vb-x, vb-y, vb-width, vb-height be the min-x, min-y, width and height values of the viewBox attribute respectively.
+    float vbX = vbRect.left;
+    float vbY = vbRect.top;
+    float vbWidth = vbRect.width();
+    float vbHeight = vbRect.height();
+
+    // Let e-x, e-y, e-width, e-height be the position and size of the element respectively.
+    float eX = eRect.left;
+    float eY = eRect.top;
+    float eWidth = eRect.width();
+    float eHeight = eRect.height();
+
+
+    // Initialize scale-x to e-width/vb-width.
+    float scaleX = eWidth / vbWidth;
+
+    // Initialize scale-y to e-height/vb-height.
+    float scaleY = eHeight / vbHeight;
+
+    // Initialize translate-x to vb-x - e-x.
+    // Initialize translate-y to vb-y - e-y.
+    float translateX = vbX - eX;
+    float translateY = vbY - eY;
+
+    // If align is 'none'
+    if (meetOrSlice == MOS_NONE) {
+      // Let scale be set the smaller value of scale-x and scale-y.
+      // Assign scale-x and scale-y to scale.
+      float scale = scaleX = scaleY = Math.min(scaleX, scaleY);
+
+      // If scale is greater than 1
+      if (scale > 1) {
+        // Minus translateX by (eWidth / scale - vbWidth) / 2
+        // Minus translateY by (eHeight / scale - vbHeight) / 2
+        translateX -= (eWidth / scale - vbWidth) / 2;
+        translateY -= (eHeight / scale - vbHeight) / 2;
+      } else {
+        translateX -= (eWidth - vbWidth * scale) / 2;
+        translateY -= (eHeight - vbHeight * scale) / 2;
+      }
+    } else {
+      // If align is not 'none' and meetOrSlice is 'meet', set the larger of scale-x and scale-y to the smaller.
+      // Otherwise, if align is not 'none' and meetOrSlice is 'slice', set the smaller of scale-x and scale-y to the larger.
+
+      if (!align.equals("none") && meetOrSlice == MOS_MEET) {
+        scaleX = scaleY = Math.min(scaleX, scaleY);
+      } else if (!align.equals("none") && meetOrSlice == MOS_SLICE) {
+        scaleX = scaleY = Math.max(scaleX, scaleY);
+      }
+
+      // If align contains 'xMid', minus (e-width / scale-x - vb-width) / 2 from transform-x.
+      if (align.contains("xMid")) {
+        translateX -= (eWidth / scaleX - vbWidth) / 2;
+      }
+
+      // If align contains 'xMax', minus (e-width / scale-x - vb-width) from transform-x.
+      if (align.contains("xMax")) {
+        translateX -= eWidth / scaleX - vbWidth;
+      }
+
+      // If align contains 'yMid', minus (e-height / scale-y - vb-height) / 2 from transform-y.
+      if (align.contains("yMid")) {
+        translateY -= (eHeight / scaleY - vbHeight) / 2;
+      }
+
+      // If align contains 'yMax', minus (e-height / scale-y - vb-height) from transform-y.
+      if (align.contains("yMax")) {
+        translateY -= eHeight / scaleY - vbHeight;
+      }
+
+    }
+
+    // The transform applied to content contained by the element is given by
+    // translate(translate-x, translate-y) scale(scale-x, scale-y).
+    Matrix transform = new Matrix();
+    transform.postTranslate(-translateX * (fromSymbol ? scaleX : 1), -translateY * (fromSymbol ? scaleY : 1));
+    transform.postScale(scaleX, scaleY);
+    return transform;
   }
 
   @Override
